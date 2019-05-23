b1761d95cfcd134d0e2010c581088e6c3318105d
Leligdowicz <oleligdowicz@deloittece.com>
2019-05-23 13:52:03 +0200

    VIES interface, adjustment for SQL Server implementation

--- CheckNIP.py
+++ CheckNIP.py
@@ -1,14 +1,12 @@
 import requests as r
 import pandas as pd
 
-
-
-#NIPdf = pd.read_csv("C:/Users/oleli/Desktop/NIPlist.txt", names=["NIP"])
-NIPdf = pd.DataFrame({"NIP":["7630003498","8730006829","1111111111"]})
-NIPdf.insert(1,"GateResponse",None)
-NIPdf.insert(2, "GateTextResponse",None)
-NIPdf.insert(3,"MFResponse",None)
-NIPdf.insert(4,"MFTextResponse",None)
+SQL_in = pd.read_csv("C:/Users/oleli/Desktop/NIPlist.txt", names=["NIP"])
+NIPdf = SQL_in
+NIPdf.insert(2,"GateResponse",None)
+NIPdf.insert(3, "GateTextResponse",None)
+NIPdf.insert(4,"MFResponse",None)
+NIPdf.insert(5,"MFTextResponse",None)
 
 #Send NIP and get response
 url = "https://sprawdz-status-vat.mf.gov.pl"
@@ -23,20 +21,18 @@ body = """ <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/enve
                  </soapenv:Body>
                 </soapenv:Envelope>
 """
+proxy ={"<past proxy here>"}
 
 for n, NIP in enumerate(NIPdf["NIP"]):
     response = r.get(url
                       ,data=(body % (NIP,))
                       ,headers=headers
+					  ,proxies=proxy
                       )
     NIPdf.at[n,"GateResponse"] = response.status_code #response http status code
     NIPdf.at[n,"GateTextResponse"] = r.status_codes._codes[response.status_code][0] #response http code description
     if response.status_code == 200:
         NIPdf.at[n, "MFResponse"] = response.text.split("<Kod>",1)[1].split("</Kod",1)[0] #response letter from mf gate
         NIPdf.at[n, "MFTextResponse"] = response.text.split("<Komunikat>",1)[1].split("</Komunikat",1)[0] #response text from mf gate
-        print( response.text.split("<Komunikat>",1)[1].split("</Komunikat",1)[0])
-    #if n == 1: break
-    #counter = n
 
-pd.set_option("display.max_columns", 7)
-print(NIPdf.head())
+SQL_out = NIPdf
--- CheckVIES.py
+++ CheckVIES.py
@@ -1,15 +1,18 @@
 import requests as r
 import pandas as pd
 
-
-
+ifproxy = 0
 #NIPdf = pd.read_csv("C:/Users/oleli/Desktop/NIPlist.txt", names=["NIP"])
 NipList = ["PL7630003498","PL8730006829","PT504220560","DE111628131","GB195929307"]
-NIPdf = pd.DataFrame({"VATUE":NipList})
-NIPdf.insert(1,"GateResponse",None)
-NIPdf.insert(2, "GateTextResponse",None)
-NIPdf.insert(3,"MFResponse",None)
-NIPdf.insert(4,"MFTextResponse",None)
+SQL_in = pd.DataFrame({"VATUE":NipList})
+SQL_in.insert(1,"RawVATUE",None)
+NIPdf = SQL_in
+NIPdf.insert(2,"GateResponse",None)
+NIPdf.insert(3, "GateTextResponse",None)
+NIPdf.insert(4,"Response",None)
+NIPdf.insert(5,"CompanyName",None)
+NIPdf.insert(6,"CompanyAddress",None)
+NIPdf.insert(7,"CompanyType",None)
 
 #Send NIP and get response
 url = "http://ec.europa.eu/taxation_customs/vies/services/checkVatService"
@@ -26,6 +29,10 @@ body = """ <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
             </s:Envelope>";
 
 """
+if ifproxy:
+    proxy ={"<past proxy here>"}
+else:
+    proxy = None
 
 for n, vatUe in enumerate(NIPdf["VATUE"]):
     prefix = vatUe[0:2]
@@ -33,16 +40,19 @@ for n, vatUe in enumerate(NIPdf["VATUE"]):
     response = r.post(url
                       ,data=(body % (prefix, NIP,))
                       ,headers=headers
+                      , proxies=proxy
                       )
     NIPdf.at[n,"GateResponse"] = response.status_code #response http status code
     NIPdf.at[n,"GateTextResponse"] = r.status_codes._codes[response.status_code][0] #response http code description
     if response.status_code == 200:
-        print(response.text)
-#TO-DO: unpack data to dataframe
-       # NIPdf.at[n, "MFResponse"] = response.text.split("<Kod>",1)[1].split("</Kod",1)[0] #response letter from mf gate
-       # NIPdf.at[n, "MFTextResponse"] = response.text.split("<Komunikat>",1)[1].split("</Komunikat",1)[0] #response text from mf gate
-       # print( response.text.split("<Komunikat>",1)[1].split("</Komunikat",1)[0])
-    # if n == 1: break
+        NIPdf.at[n, "Response"] = response.text.split("<valid>",1)[1].split("</valid>",1)[0]
+        if "</traderName>" in response.text:
+            NIPdf.at[n, "CompanyName"] = response.text.split("<traderName>", 1)[1].split("</traderName>", 1)[0]
+        if "</traderAddress>" in response.text:
+            NIPdf.at[n, "CompanyAddress"] = response.text.split("<traderAddress>", 1)[1].split("</traderAddress>", 1)[0]
+        if "</traderCompanyType>" in response.text:
+            NIPdf.at[n, "CompanyType"] = response.text.split("<traderCompanyType>", 1)[1].split("</traderCompanyType>", 1)[0]
+        if NIPdf.at[n, "CompanyType"] == "---":
+            NIPdf.at[n, "CompanyType"] = None
 
-pd.set_option("display.max_columns", 7)
-print(NIPdf.head())
+SQL_out = NIPdf
